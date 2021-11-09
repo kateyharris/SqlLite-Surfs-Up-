edf3a8b21e21c5abf8c416dea8faae3891049c12
Katey Harris <kateyharris9998@gmail.com>
2021-11-09 16:00:43 -0700

    Final

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # surfs_up
+
+## _**Overview**_ 
+In order to determine if the surf and ice cream shop buisness is sutainable year-round, the temperatures for the months of June and December in Oahu is analyzed.
+
+## _**Results**_
+_**Temperatures in Oahu in June, and in December are beautiful. There doesn't appear to be any reason any business would not be sustainable year-round.**_<br\>
+- Temperatures in June Average 75 degrees, and temperatures in December are just a tad cooler, with an Average of 71 degrees.
+- Temperatures in June have gotten as low as 64 degree, where as in December it has gotten down to 56 degrees
+- The dataset did provide more temperatures for June than December, but still feel the 1,517 days in December is a sufficent dataset for this analysis.
+
+## _**Summary**_
+Provide a high-level summary of the results and two additional queries that you would perform to gather more weather data for June and December.
+I do reccommend looking into other weather patterns, such as percipitation, and wind. Hurricans can put a damper on things for sure.
+In Addition, analytics should be done for all months to get a true picture of the year-round sustainability.
---
+++ app.py
@@ -0,0 +1,88 @@
+# Dependencies
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+# Set up database engine
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect the database into our classes
+Base = automap_base()
+
+# reflect the database tables
+Base.prepare(engine, reflect=True)
+
+# save our references to each table.
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# create a session link from Python to our database
+session = Session(engine)
+
+# Set up FLASK
+app = Flask(__name__)
+
+# Set up Route
+@app.route('/')
+
+# define the welcome route
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
+# build route for the precipitation analysis
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= prev_year).all()
+   precip = {date: prcp for date, prcp in precipitation}
+   return jsonify(precip)
+
+# build route for the stations
+@app.route("/api/v1.0/stations")
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
+
+# build route for the temperature observations
+@app.route("/api/v1.0/tobs")
+def temp_monthly():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).\
+      filter(Measurement.station == 'USC00519281').\
+      filter(Measurement.date >= prev_year).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+
+# build route to report on the minimum, average, and maximum temperatures
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    if not end:
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps)
